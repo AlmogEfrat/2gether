27549b49e4bce025d6f76d88f9982e2567088cad
AlmogEfrat <almog4400568@gmail.com>
2024-07-12 14:24:11 +0300

    update modal

--- client/src/pages/OnBoarding.js
+++ client/src/pages/OnBoarding.js
@@ -1,10 +1,27 @@
-import Nav from "../components/Nav";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Nav from "../components/Nav"; // Make sure to import Nav if it's in a different file
 
 const OnBoarding = () => {
+  const interestsArray = [
+    "Music",
+    "Sports",
+    "Travel",
+    "Movies",
+    "Books",
+    "Fitness",
+    "Gaming",
+    "Cooking",
+    "Art",
+    "Technology",
+    "Photography",
+    "Nature",
+    "Science",
+    "History",
+    "Fashion",
+  ];
   const [cookies, setCookie, removeCookie] = useCookies(null);
   const [formData, setFormData] = useState({
     user_id: cookies.UserId,
@@ -19,6 +36,10 @@ const OnBoarding = () => {
     about: "",
     matches: [],
   });
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
 
   let navigate = useNavigate();
 
@@ -80,7 +101,6 @@ const OnBoarding = () => {
                 value={formData.dob_day}
                 onChange={handleChange}
               />
-
               <input
                 id="dob_month"
                 type="number"
@@ -90,11 +110,10 @@ const OnBoarding = () => {
                 value={formData.dob_month}
                 onChange={handleChange}
               />
-
               <input
                 id="dob_year"
                 type="number"
-                name="dob_year" // השם כמו בבסיס נתונים שיצרנו
+                name="dob_year"
                 placeholder="YYYY"
                 required={true}
                 value={formData.dob_year}
@@ -107,7 +126,7 @@ const OnBoarding = () => {
               <input
                 id="man-gender-identity"
                 type="radio"
-                name="gender_identity" // like mongo
+                name="gender_identity"
                 value="man"
                 onChange={handleChange}
                 checked={formData.gender_identity === "man"}
@@ -116,7 +135,7 @@ const OnBoarding = () => {
               <input
                 id="woman-gender-identity"
                 type="radio"
-                name="gender_identity" // like mongo
+                name="gender_identity"
                 value="woman"
                 onChange={handleChange}
                 checked={formData.gender_identity === "woman"}
@@ -134,7 +153,6 @@ const OnBoarding = () => {
             </div>
 
             <label htmlFor="show-gender">Show Gender on my Profile</label>
-
             <input
               id="show-gender"
               type="checkbox"
@@ -144,7 +162,6 @@ const OnBoarding = () => {
             />
 
             <label>Show Me</label>
-
             <div className="multiple-input-container">
               <input
                 id="man-gender-interest"
@@ -174,6 +191,15 @@ const OnBoarding = () => {
               />
               <label htmlFor="everyone-gender-interest">Everyone</label>
             </div>
+            <button
+              data-modal-target="default-modal"
+              data-modal-toggle="default-modal"
+              className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+              onClick={handleOpenModal}
+            >
+              Interests
+            </button>
 
             <label htmlFor="about">About me</label>
             <input
@@ -186,14 +212,10 @@ const OnBoarding = () => {
               onChange={handleChange}
             />
 
-            <input type="submit" />
-          </section>
-
-          <section>
             <label htmlFor="url">Profile Photo</label>
             <input
               type="url"
-              name="url" //like DB
+              name="url"
               id="url"
               onChange={handleChange}
               required={true}
@@ -203,10 +225,84 @@ const OnBoarding = () => {
                 <img src={formData.url} alt="profile pic preview" />
               )}
             </div>
+
+            <button
+              type="submit"
+              className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
           </section>
+
+          {openModal && (
+            <div
+              id="default-modal"
+              tabIndex="-1"
+              aria-hidden="true"
+              className="fixed inset-0 flex items-center justify-center z-50 overflow-y-auto"
+            >
+              <div className="relative w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                  <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                  </h3>
+                  <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={handleCloseModal}
+                  >
+                    <svg
+                      className="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                </div>
+
+                <div className="flex flex-wrap justify-between max-w-full gap-3 mx-5 p-3">
+                  {interestsArray.map((category, index) => (
+                    <div key={index} className="">
+                      <button className="p-1 bg-white border border-pink-500 rounded-md cursor-pointer text-center">
+                        {category}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <button
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={handleCloseModal}
+                  >
+                    I accept
+                  </button>
+                  <button
+                    type="button"
+                    className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    onClick={handleCloseModal}
+                  >
+                    Decline
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </>
   );
 };
+
 export default OnBoarding;
